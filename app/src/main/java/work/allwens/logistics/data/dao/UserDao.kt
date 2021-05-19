e23230992266dac03b4bee94919e21573fde6155
amtoaer <amtoaer@gmail.com>
2021-05-19 23:02:08 +0800

    fix(dao): fix app crash

--- app/src/main/java/work/allwens/logistics/data/dao/UserDao.kt
+++ app/src/main/java/work/allwens/logistics/data/dao/UserDao.kt
@@ -9,8 +9,8 @@ import work.allwens.logistics.data.model.User
 @Dao
 interface UserDao {
 
-    @Query("SELECT * FROM t_user WHERE user_login = :account AND user_passwd = :passwd")
-    fun findByUserInfo(account: String, passwd: String): User
+    @Query("SELECT * FROM t_user WHERE user_account = :account AND user_passwd = :passwd")
+    suspend fun findByUserInfo(account: String, passwd: String): User
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(user: User)
