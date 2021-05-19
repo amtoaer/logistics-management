f7ef3364ea05b654daa05a830bc2464e2b513547
amtoaer <amtoaer@gmail.com>
2021-05-19 23:04:07 +0800

    style: reformat code

--- app/src/main/java/work/allwens/logistics/ui/BaseActivity.kt
+++ app/src/main/java/work/allwens/logistics/ui/BaseActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 // 为了方便维护用户activity集合而编写的公共基类，其他activity均继承于该父类
-open class BaseActivity:AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Helper.addActivity(this)
--- app/src/main/java/work/allwens/logistics/ui/Helper.kt
+++ app/src/main/java/work/allwens/logistics/ui/Helper.kt
@@ -1,26 +1,30 @@
 package work.allwens.logistics.ui
 
 import android.util.ArraySet
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 
 // 维护activity集合的辅助单例类
 object Helper {
     // 为了方便管理，选用了集合
     private val activityList = ArraySet<AppCompatActivity>()
+
     // 添加activity
-    fun addActivity(item:AppCompatActivity){
+    fun addActivity(item: AppCompatActivity) {
         activityList.add(item)
     }
+
     // 移除activity并销毁
-    fun removeActivity(item:AppCompatActivity){
-        if (activityList.contains(item)){
+    fun removeActivity(item: AppCompatActivity) {
+        if (activityList.contains(item)) {
             activityList.remove(item)
             item.finish()
         }
     }
+
     // 退出程序
-    fun exitTheApp(){
-        for (item in activityList){
+    fun exitTheApp() {
+        for (item in activityList) {
             item.finish()
         }
     }
